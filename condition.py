dbed705ecae4e5577b64bcda8136e09ad20c960e
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-14 16:56:14 +0530

    elif

--- condition.py
+++ condition.py
@@ -2,7 +2,9 @@ age = input("Please enter your age ")
 age =int(age)
 
 if age >18:  #typecating "10" = 10
+    #True Block
     print("you can vote")
 
 else:
+    #False Block
     print("you can't vote")    
---
+++ elif.py
@@ -0,0 +1,8 @@
+age = input('Please enter your age ')
+age = int(age)
+
+if age >=20 and age <=25:
+    print('your age between 20 to 25 year old ')
+
+elif age >=26 and age <=30:
+    print("your age between 26 to 30 year old ")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
